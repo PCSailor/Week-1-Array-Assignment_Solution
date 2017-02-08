158243c5d15275f4844f899e3ee6f5270d6f5d2b
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-08 10:48:27 -0600

    2nd

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var wordArray = '["ASDF", "BLAH", "Oh! ", "Hi ", "There!"]';
 var stringResult = " "; // start with an empty stringResult
 var stringResult = "Taco";
+
+function putAllOfTheWordsTogether() {
 // concatenate all of the words
 for (var i = 0; i < wordArray.length; i++) {
   console.log(stringResult);
   stringResult = stringResult + wordArray[i]; // Add a word to the stringResult
   console.log(stringResult);
+}
 }
 console.log(stringResult);
